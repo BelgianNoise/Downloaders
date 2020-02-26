0366801c6c45a1e9e33a65a3ef6d13a3c092bb80
BelgianNoise <king.arthur360@gmail.com>
2020-02-26 18:21:40 +0100

    login

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ This script requires at least 1 parameter:
 * 1: The link to the vrt.nu page
 * 2: (Optional) The path you want to download the videos to. This defaults to '/mnt/h/Films' for my own convenience.
 
+The script will ask you for your vrt.nu login creds which should be entered like this(mind the ' ):
+- username: 'email@example.com'
+- password: 'yourPassword'
+
 !!! This script requires you to have youtube-dl installed (apt install youtube-dl) !!!
--- vrtSeasonDownload.py
+++ vrtSeasonDownload.py
@@ -9,6 +9,9 @@ if len(sys.argv) > 1:
 else:
     sys.exit("This script requires at least 1 parameter:\n   1: The link to the vrt.nu page\n   2: (Optional) The path you want to download the videos to. This defaults to '"+path+"' for my own convenience.\n\n !!! This script requires you to have youtube-dl installed (apt install youtube-dl) !!!")
 
+username = str(input('username: '))
+password = str(input('password: '))
+
 response = requests.get(url)
 contents = response.content
 matches = re.findall(r'<nui-tile href="(.*?)"', contents)
@@ -20,6 +23,6 @@ for m in matches:
 os.chdir(path)
 for m in matches:
     print("Downloading: '" + re.findall(r'/([^/]*)/$', m)[0] + "'...")
-    command = "youtube-dl vrt.be" + m 
+    command = "youtube-dl vrt.be" + m + " --username "+username+" --password "+password 
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
