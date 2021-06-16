766d17712bc5ae5957da1cd92caef6b0625999e6
Arthur Joppart <arthur@digita.ai>
2021-06-17 00:11:58 +0200

    feat: added more routes

--- DownloadQ/routes.py
+++ DownloadQ/routes.py
@@ -7,4 +7,7 @@ class ROUTES(Enum):
   GET_ALL     = '^/get/all$'
   ADD         = '^/add$'
   DELETE      = '^/delete/[^/]*$'
+  DELETE_ALL  = '^/delete/all$'
+  FINISH      = '^/finish/[^/]*$'
+  FINISH_ALL  = '^/finish/all*$'
   FAVICON     = '^/favicon.ico$'
